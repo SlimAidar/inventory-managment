17d694fd8da21d5aae5755efeb3dfdb10a3e9029
slimaidar <slimaidarismail@gmail.com>
2019-03-20 21:54:56 +0100

    create inventory main models

---
+++ inventory/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name		
+
+
+class Item(models.Model):
+	name 	 = models.CharField(max_length=50)
+	category = models.ForeignKey(Category,on_delete=models.CASCADE)
+	quantity = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.name
+
+
+class Client(models.Model):
+	place		= models.CharField(max_length=50)
+	description = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.place
+	
+
+class Transaction(models.Model):
+	quantity = models.IntegerField()
+	time 	 = models.DateTimeField(auto_now=True)
+	item 	 = models.ForeignKey(Item, on_delete=models.CASCADE,blank=False, null=False)
+	Client 	 = models.ForeignKey(Client, on_delete=models.CASCADE,blank=False, null=False)
