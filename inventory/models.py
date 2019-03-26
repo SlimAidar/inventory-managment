5d907a11f5eabdafaaf0432708c766ea6e700b7d
slimaidar <slimaidarismail@gmail.com>
2019-03-26 09:40:12 +0100

    remove client model

--- inventory/models.py
+++ inventory/models.py
@@ -17,19 +17,12 @@ class Item(models.Model):
 		return self.name
 
 
-class Client(models.Model):
-	place		= models.CharField(max_length=50)
-	description = models.CharField(max_length=100)
-
-	def __str__(self):
-		return self.place
-	
 
 class Transaction(models.Model):
 	quantity = models.IntegerField()
 	time 	 = models.DateTimeField(auto_now=True)
 	item 	 = models.ForeignKey(Item, on_delete=models.CASCADE,blank=False, null=False)
-	Client 	 = models.ForeignKey(Client, on_delete=models.CASCADE,blank=True, null=True)
+	Client 	 = models.CharField(max_length=300, blank=True, null=True)
 
 	class Meta:
 		ordering = ['-id']
